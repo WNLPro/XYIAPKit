3897b45e7ab4e44431656ec7e1bdab125bbc7627
fn007 <fstnet007@gmail.com>
2021-12-22 10:01:08 +0800

    fix dispatch

--- XYIAPKit/Core/XYStore.m
+++ XYIAPKit/Core/XYStore.m
@@ -598,8 +598,9 @@ typedef void (^XYStoreSuccessBlock)(void);
 
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         if ([self isTransResotred:transaction]) {
-            [self didVerifyTransaction:transaction queue:queue];
-
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self didVerifyTransaction:transaction queue:queue];
+            });
             return;
         }
 
